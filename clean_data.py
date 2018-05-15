ac26933769285d0ce32c65400f040e593f5bbe1e
raopg <raopg@uci.edu>
2018-05-14 21:24:46 -0700

    Standalone version update

--- clean_data.py
+++ clean_data.py
@@ -1,7 +1,8 @@
+# -*- coding : utf -8 -*-
 import pandas as py
 #First, we import the data from the csv file included
-def clean_data(csv_filename = ''):
-    df = pd.read_csv('olympics.csv', index_col = 0, skiprows=1)
+def clean_data(csv_filename = 'olympics.csv'):
+    df = pd.read_csv(csv_filename, index_col = 0, skiprows=1)
 
     for col in df.columns():
         if col[:2] == '01':
@@ -10,7 +11,7 @@ def clean_data(csv_filename = ''):
             df.rename(columns={col:'Silver'+col[4:]}, inplace = True)
         if col[:2] == '03':
             df.rename(columns={col:'Bronze'+col[4:]}, inplace = True)
-        if col[:2] == '№':
+        if col[:1]:
             df.rename(columns={col:'#'+col[1:]}, inplace = True)
 
     names_ids =  df.index.str.split('\s\(')
@@ -21,6 +22,19 @@ def clean_data(csv_filename = ''):
     print(df.head) # Print the first few elements to ensure that this data has been cleaned and is ready for use.
     return df 
     
+if __name__ = "__main__":
+    print("-------------------------------- \n")
+    df = None
+    print('This program cleans the Olympics data for all countries which has participated in Summer and Winter Olympics \n')
+    filename = input('Enter the CSV file that needs to be cleaned, default is \'olympics.csv\' \n')
+    if filename == '':
+        clean_data()
+    else:
+        df = clean_data(filename)
+
+
+
+
 
     
     
